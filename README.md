df89f71ae3f5deaefaf8328301853a639dfd1f83
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2024-03-11 13:07:24 -0300

    Example for ben

---
+++ README.md
@@ -0,0 +1,3 @@
+## How
+
+Run `npm start` to run with ts-node loader or `npm run build-start` to `tsc` and then run `node dist/index.js`
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {UniversalAddress} from "@wormhole-foundation/sdk-definitions";
+
+const addr = new UniversalAddress("0x38035f4c1e1772d43a3535535ea5b29c1c3ab2c0026d4ad639969831bd1d174d", "hex")
+console.log(addr.toString());
